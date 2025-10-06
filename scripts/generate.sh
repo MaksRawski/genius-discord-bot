c25475d91a64b3899df69074b9d2aa23af864377
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-10-06 12:09:31 +0200

    fix: scripts are working again

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,46 +1,66 @@
 #!/bin/bash
 # requires Lato font
 
-export LANG=en_US.UTF-8
-export LANGUAGE=en.UTF-8
+set -e
+trap 'echo Error on line $LINENO ' ERR
 
-img=$1
+img="$1"
 caption="$(echo "$2" | fold -sw 34)"
-author="$(echo "$3" | PERLIO=:utf8 perl -pe '$_=uc')"
-track_name="$(echo "$4" | PERLIO=:utf8 perl -pe '$_=uc')"
-info="$author \\\"$track_name\\\""
+author="$(echo "$3" | perl -pe 'use utf8; $_=uc')"
+track_name="$(echo "$4" | perl -pe 'use utf8; $_=uc')"
+info="$author \"$track_name\""
 font="Lato-Bold"
 
 out="${5:-out.png}"
 
-if ! [[ -f "$1" ]] || [ $# -lt 4 ] || [ $# -gt 5 ]; then
+if [ $# -lt 4 ] || [ $# -gt 5 ]; then
 	echo "Usage $0 cover.png caption author track_name [out.png]"
 	exit 1
 fi
 
-cwd="$(pwd)"
-dir=$(mktemp -d)
-cp scripts/quote.png $dir
-cp "$img" $dir
-cd $dir
+if [ ! -f "$1" ]; then
+	echo "No such file: $1"
+	exit 1
+fi
+
+output_dir="$(pwd)"
+script_dir=$(dirname "$(readlink -f "$0")")
+tmp_dir=$(mktemp -d)
+if [ ! -f "$script_dir/quote.png" ]; then
+	echo "quote.png must be in the same directory as generate.sh!"
+	exit 1
+fi
+cp "$script_dir/quote.png" "$tmp_dir"
+input_img="$tmp_dir/input.${img##*.}"
+cp "$img" "$input_img"
+cd "$tmp_dir"
+
+MAGICK_CMD=magick
+if ! command -v $MAGICK_CMD &> /dev/null; then
+	MAGICK_CMD=convert
+fi
+if ! command -v $MAGICK_CMD &> /dev/null; then
+	echo "ImageMagick not installed!"
+	exit 1
+fi
 
 # scale the image and remove any metadata
-if [ $(identify -format '%w' "$img") -gt $(identify -format '%h' "$img") ]; then
-	convert $img +profile "*" -resize x600 resized.png
+if [ $(identify -format '%w' "$input_img") -gt $(identify -format '%h' "$input_img") ]; then
+	$MAGICK_CMD "$input_img" +profile "*" -resize x600 resized.png
 else
-	convert $img +profile "*" -resize 900x resized.png
+	$MAGICK_CMD "$input_img" +profile "*" -resize 900x resized.png
 fi
 
 # crop the image to be 900x600
-convert resized.png -gravity center -extent 900x600 cropped.png
+$MAGICK_CMD resized.png -gravity center -extent 900x600 cropped.png
 
 # darken the image
-convert cropped.png -brightness-contrast -20x-25 darkened.png
+$MAGICK_CMD cropped.png -brightness-contrast -20x-25 darkened.png
 
 # add author
-convert darkened.png -gravity SouthWest \
-	-pointsize 32 -fill white -font $font \
-	-draw "text 90,52 \"$info\"" captioned.png
+$MAGICK_CMD darkened.png -gravity SouthWest \
+	-pointsize 32 -fill white -font "$font" \
+	-draw "text 90,52 '$info'" captioned.png
 
 i=$(( $(echo "$caption" | wc -l)-1 ))
 
@@ -49,22 +69,22 @@ composite -geometry 42x42+25+$((405-$i*80)) quote.png captioned.png captioned.pn
 
 echo "$caption" | while read bar ; do
 	# create text
-	convert -background white -bordercolor white \
-		-pointsize 50 -fill white -font $font \
+	$MAGICK_CMD -background white -bordercolor white \
+		-pointsize 50 -fill white -font "$font" \
 		-gravity South label:"$bar" -extent x68 -border 5x0 \
 		-fill black -annotate +0+5 "$bar" \
 		caption.png
 
 	# add bar
-	convert captioned.png caption.png -geometry +90+$((405-$i*80)) -composite captioned.png
-	i=$(($i-1))
+	$MAGICK_CMD captioned.png caption.png -geometry +90+$((405-i*80)) -composite captioned.png
+	i=$((i-1))
 done 
 
 
 # compress
-# convert captioned.png -strip -interlace Plane -gaussian-blur 0.02 -quality 75%  "$out"
-cp captioned.png "$out"
+# $MAGICK_CMD captioned.png -strip -interlace Plane -gaussian-blur 0.02 -quality 75% compressed.png
+cp captioned.png "$output_dir/$out"
+echo "Card generated succesfully! Saved as $out"
 
-cp "$out" "$cwd"
-cd "$cwd"
-rm -r $dir
+cd "$output_dir"
+rm -r "$tmp_dir"
--- scripts/genius-dl.sh
+++ scripts/genius-dl.sh
@@ -6,6 +6,14 @@
 # - fzf
 # - GENIUS_TOKEN (CLIENT ACCESS TOKEN from https://genius.com/api-clients)
 
+set -e
+trap 'echo Error on line $LINENO ' ERR
+
+if [ ! -v GENIUS_TOKEN ]; then
+	echo "GENIUS_TOKEN not set, exiting."
+	exit 1;
+fi
+
 echo -n "Type song, album or artist name: "
 read query
 
@@ -15,12 +23,15 @@ query_json=$(curl -s -G \
 	https://api.genius.com/search
 )
 
-result=$(echo "$query_json" | jq '.response.hits[] | "\(.result.id): \(.result.artist)-\(.result.title)"' -r | fzf --layout=reverse)
-song_id=$(echo $result | cut -d : -f 1)
-song_name=$(echo $result | cut -d : -f 2 | xargs)
+echo "$query_json" > a.json
+result=$(echo "$query_json" | jq '.response.hits[] | "\(.result.id): \(.result.artist_names) - \(.result.title)"' -r | fzf)
+song_id=$(echo "$result" | cut -d : -f 1)
 
-img=$(curl -s -H "Authorization: Bearer $GENIUS_TOKEN" \
-	https://api.genius.com/songs/$song_id | jq '.response.song.header_image_url' -r
-)
+song_json=$(curl -s -H "Authorization: Bearer $GENIUS_TOKEN" https://api.genius.com/songs/$song_id)
+echo "$song_json" > asdf.json
+img_url=$(echo "$song_json" | jq '.response.song.song_art_image_url' -r)
 
-curl -s "$img" -o "$song_name.jpg"
+ext="${img_url##*.}"
+file_name="$(echo $song_json | jq '.response.song.artist_names + " - " + .response.song.title + ".'"$ext"'"' -r)"
+curl "$img_url" -o "$file_name"
+echo Succesfully saved as "$file_name"
